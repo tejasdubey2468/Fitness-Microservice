f729cee013fc79fd936626775120aec988195393
Tejas Dubey <tejasdubey2468@gmail.com>
2026-09-23 23:15:27 +0530

    Controller layers and Api testing

---
+++ activityservice/src/main/java/com/fitness/activityservice/controller/RecommendationController.java
@@ -0,0 +1,29 @@
+package com.fitness.activityservice.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/recommendation")
+public class RecommendationController {
+    private final RecommendationService recommendationService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation (@PathVariable String userId){
+        return ResponseEntity.ok(recommendationService.getUserRecommendation(userId));
+    }
+
+    @GetMapping("/activity/{activityId}")
+    public ResponseEntity<Recommendation> getActivityRecommendation (@PathVariable String activityId){
+        return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
+    }
+
+}
